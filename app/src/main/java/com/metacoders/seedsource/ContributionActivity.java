5f89a8381e8d88653891010c6d78ba935bfe290f
shaheensms <shaheensms111@gmail.com>
2019-10-19 11:36:41 +0600

    UI design

---
+++ app/src/main/java/com/metacoders/seedsource/ContributionActivity.java
@@ -0,0 +1,14 @@
+package com.metacoders.seedsource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContributionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contribution);
+    }
+}
---
+++ app/src/main/java/com/metacoders/seedsource/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.metacoders.seedsource;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
